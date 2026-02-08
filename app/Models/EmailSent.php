ea33ae6a50d607fa53142d604b16dc07494797e9
Regis Freyd <djaiss@users.noreply.github.com>
2026-02-07 23:31:23 -0500

    wip

--- app/Models/EmailSent.php
+++ app/Models/EmailSent.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use Illuminate\Support\Carbon;
 use Database\Factories\EmailSentFactory;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
 /**
  * @property int $id
--- database/migrations/2026_02_08_033717_create_emails_sent_table.php
+++ database/migrations/2026_02_08_033717_create_emails_sent_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
             $table->datetime('delivered_at')->nullable();
             $table->datetime('bounced_at')->nullable();
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('user_id')->references('id')->on('users')->nullOnDelete();
         });
     }
 
