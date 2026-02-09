ce5fb57ec2c8870bb64a6011e987297b133b27c6
Regis Freyd <djaiss@users.noreply.github.com>
2026-02-08 21:39:58 -0500

    feat: add methods to get user initials and full name

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 /**
  * Class User
@@ -92,5 +93,28 @@ class User extends Authenticatable implements MustVerifyEmail
         return $this->belongsToMany(Organization::class)
             ->withPivot(['joined_at'])
             ->withTimestamps();
+    }
+
+    /**
+     * Get the user's initials
+     */
+    public function initials(): string
+    {
+        return Str::of($this->first_name . ' ' . $this->last_name)
+            ->explode(' ')
+            ->map(fn(string $name) => Str::of($name)->substr(0, 1))
+            ->implode('');
+    }
+
+    /**
+     * Get the user's full name by combining first and last name.
+     */
+    public function getFullName(): string
+    {
+        $firstName = $this->first_name;
+        $lastName = $this->last_name;
+        $separator = $firstName && $lastName ? ' ' : '';
+
+        return $firstName . $separator . $lastName;
     }
 }
