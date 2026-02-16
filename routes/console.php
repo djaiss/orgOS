f6ee5e69b631bc6ef491aac4153dc6b3437d0c54
Regis Freyd <djaiss@users.noreply.github.com>
2026-02-15 19:10:46 -0500

    feat: setup auto delete accounts

--- routes/console.php
+++ routes/console.php
@@ -1,3 +1,11 @@
 <?php
 
 declare(strict_types=1);
+
+use Illuminate\Support\Facades\Schedule;
+use App\Jobs\DeleteInactiveAccounts;
+
+Schedule::job(
+    new DeleteInactiveAccounts(),
+    'low',
+)->dailyAt('00:30');
