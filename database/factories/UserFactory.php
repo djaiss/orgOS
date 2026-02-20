1bbaac9312376177438c47e32abd446079df0e4a
Regis Freyd <djaiss@users.noreply.github.com>
2026-02-20 10:19:17 -0500

    wip

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,7 +17,7 @@ class UserFactory extends Factory
     /**
      * The current password being used by the factory.
      */
-    private static ?string $password;
+    private static string $password;
 
     /**
      * Define the model's default state.
