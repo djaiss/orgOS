d981dfe6a777b5110fcd114bc6c18100859e2290
Regis Freyd <djaiss@users.noreply.github.com>
2026-03-02 13:32:20 -0500

    feat: concept of address

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Interfaces\HasAddress;
 use Carbon\Carbon;
 use Database\Factories\OfficeFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -27,7 +29,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @property Carbon $created_at
  * @property Carbon|null $updated_at
  */
-class Office extends Model
+class Office extends Model implements HasAddress
 {
     /** @use HasFactory<OfficeFactory> */
     use HasFactory;
@@ -95,5 +97,61 @@ class Office extends Model
     public function officeType(): BelongsTo
     {
         return $this->belongsTo(OfficeType::class);
+    }
+
+    /**
+     * Get the first line of the address of the office.
+     */
+    public function getAddressLine1(): ?string
+    {
+        return $this->address_line_1;
+    }
+
+    /**
+     * Get the second line of the address of the office.
+     */
+    public function getAddressLine2(): ?string
+    {
+        return $this->address_line_2;
+    }
+
+    /**
+     * Get the city of the office.
+     */
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    /**
+     * Get the state or province of the office.
+     */
+    public function getStateProvince(): ?string
+    {
+        return $this->state_province;
+    }
+
+    /**
+     * Get the postal code of the office.
+     */
+    public function getPostalCode(): ?string
+    {
+        return $this->postal_code;
+    }
+
+    /**
+     * Get the country name of the office.
+     */
+    public function getCountryName(): ?string
+    {
+        return $this->country?->name;
+    }
+
+    /**
+     * Get the full address of the office.
+     */
+    protected function address(): Attribute
+    {
+        return Attribute::get(fn(): Address => Address::fromModel($this));
     }
 }
