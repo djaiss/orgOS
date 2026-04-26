9fdb01018133cb7a50e441d531a1570d5ffecb50
Regis Freyd <djaiss@users.noreply.github.com>
2026-04-26 18:37:41 -0400

    feat: ui to manage office types

--- resources/views/app/organization/adminland/offices/manage.blade.php
+++ resources/views/app/organization/adminland/offices/manage.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout :organization="$organization">
   <x-slot:title>
-    {{ __('Members') }}
+    {{ __('Offices') }}
   </x-slot>
 
   <x-breadcrumb :items="[
@@ -9,20 +9,17 @@
     ['label' => __('Offices')]
   ]" />
 
-  <!-- settings layout -->
   <div class="grid grow bg-gray-50 sm:grid-cols-[220px_1fr] dark:bg-gray-950">
-    <!-- Sidebar -->
     @include('app.organization.adminland._sidebar')
 
-    <!-- Main content -->
     <section class="p-4 sm:p-8">
       <div class="mx-auto max-w-5xl space-y-6 sm:px-0">
-        @include('app.organization.adminland.offices._office_types', [
-          'officeTypes' => $officeTypes,
-        ])
-
         @include('app.organization.adminland.offices._offices', [
           'offices' => $offices,
+        ])
+
+        @include('app.organization.adminland.offices._office_types', [
+          'officeTypes' => $officeTypes,
         ])
       </div>
     </section>
