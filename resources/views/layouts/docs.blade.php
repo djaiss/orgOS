809594c2e466cd11d49224a7c5b9dfb050c1b6d8
Regis Freyd <djaiss@users.noreply.github.com>
2026-05-01 21:28:20 -0400

    feat: add api method to get members

--- resources/views/layouts/docs.blade.php
+++ resources/views/layouts/docs.blade.php
@@ -20,6 +20,9 @@
           officesDocumentation:
             '{{ request()->routeIs('marketing.docs.api.organizations.offices.*') ? 'true' : 'false' }}' ===
             'true',
+          membersDocumentation:
+            '{{ request()->routeIs('marketing.docs.api.organizations.members.*') ? 'true' : 'false' }}' ===
+            'true',
         }" class="bg-light dark:bg-dark z-10 pt-16">
           <!-- api documentation -->
           <div @click="openApiDocumentation = !openApiDocumentation" class="mb-2 flex cursor-pointer items-center justify-between rounded-md border border-transparent px-2 py-1 hover:border-gray-200 hover:bg-blue-50 dark:hover:border-gray-700 dark:hover:bg-gray-800">
@@ -46,16 +49,19 @@
               </div>
 
               <!-- adminland -->
-              <div @click.stop="officeTypesDocumentation = !officeTypesDocumentation; officesDocumentation = !officesDocumentation" class="flex cursor-pointer items-center justify-between rounded-md border border-transparent px-2 py-1 pl-3 text-xs text-gray-500 uppercase hover:border-gray-200 hover:bg-blue-50 dark:text-gray-400 dark:hover:border-gray-700 dark:hover:bg-gray-800">
+              <div @click.stop="officeTypesDocumentation = !officeTypesDocumentation; officesDocumentation = !officesDocumentation; membersDocumentation = !membersDocumentation" class="flex cursor-pointer items-center justify-between rounded-md border border-transparent px-2 py-1 pl-3 text-xs text-gray-500 uppercase hover:border-gray-200 hover:bg-blue-50 dark:text-gray-400 dark:hover:border-gray-700 dark:hover:bg-gray-800">
                 <h3>Adminland</h3>
-                <x-phosphor-caret-right x-bind:class="officeTypesDocumentation || officesDocumentation ? 'rotate-90' : ''" class="h-4 w-4 text-gray-500 transition-transform duration-300" />
+                <x-phosphor-caret-right x-bind:class="officeTypesDocumentation || officesDocumentation || membersDocumentation ? 'rotate-90' : ''" class="h-4 w-4 text-gray-500 transition-transform duration-300" />
               </div>
-              <div x-show="officeTypesDocumentation || officesDocumentation" class="flex flex-col gap-y-2">
+              <div x-show="officeTypesDocumentation || officesDocumentation || membersDocumentation" class="flex flex-col gap-y-2">
                 <div>
                   <a href="{{ route('marketing.docs.api.organizations.officetypes.index') }}" class="{{ request()->routeIs('marketing.docs.api.organizations.officetypes.index') ? 'border-l-blue-400' : 'border-l-transparent' }} block border-l-3 pl-6 hover:border-l-blue-400 hover:underline">Office Types</a>
                 </div>
                 <div>
                   <a href="{{ route('marketing.docs.api.organizations.offices.index') }}" class="{{ request()->routeIs('marketing.docs.api.organizations.offices.index') ? 'border-l-blue-400' : 'border-l-transparent' }} block border-l-3 pl-6 hover:border-l-blue-400 hover:underline">Offices</a>
+                </div>
+                <div>
+                  <a href="{{ route('marketing.docs.api.organizations.members.index') }}" class="{{ request()->routeIs('marketing.docs.api.organizations.members.index') ? 'border-l-blue-400' : 'border-l-transparent' }} block border-l-3 pl-6 hover:border-l-blue-400 hover:underline">Members</a>
                 </div>
               </div>
             </div>
